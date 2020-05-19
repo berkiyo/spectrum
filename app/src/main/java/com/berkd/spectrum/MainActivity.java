56969880c8d74024e9b604d96727caed2a824bdf
berkcodes <!Kr3b@99github.c0m##>
2020-05-19 20:47:13 +1000

    GraphView fully implemented.
    - Added a freeze button
    - Added update speed
    - Added data point precision
    - Fixed lot's of bugs

--- app/src/main/java/com/berkd/spectrum/MainActivity.java
+++ app/src/main/java/com/berkd/spectrum/MainActivity.java
@@ -2,26 +2,22 @@ package com.berkd.spectrum;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,28 +28,27 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Random;
 
-import static com.berkd.spectrum.R.id.textFrequency;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer player;
-    private String audioFilePicked; // For the dropdown spinner (audioFile spinner)
 
     private Handler handler;
     private Recorder recorder;
     private AudioCalculator audioCalculator;
 
     private TextView textAmplitude;
-    private TextView textDecibel;
     private TextView textFrequency;
-    private TextView textStatus;
-    private TextView textAverageFreq;
-    private TextView textAverageAmp;
-    private int count = 100;
 
-    private int[] amplitudeVals = new int[count];        // Store the amplitude values (recording only)
-    private double[] frequencyVals = new double[count];
+    private Spinner spinner_datarate;
+    private Spinner spinner_audiofile;
+    private Spinner spinner_samplespeed;
 
+    private int speed = 10;
+    private int audioFilePicked = 1; // For the dropdown spinner (audioFile spinner)
+    private int myDataPoints = 50; // used to set the precision of the amplitude accuracy.
+
+    private boolean buttonFreezeClicked = false;
 
     private final Handler mHandler = new Handler();
     private Runnable mTimer;
@@ -66,26 +61,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Initialise the spinners
+         */
+        spinner_datarate = findViewById(R.id.spinner_datarate);
+        initDataRateSpinner();
+        spinner_audiofile = findViewById(R.id.spinner_audiofile);
         initAudioFileSpinner();
-        initAudioSamplesSpinner();
-        constructionDialog();
+        spinner_samplespeed = findViewById(R.id.spinner_samplespeed);
+        initSampleSpeedSpinner();
 
+
+
+        constructionDialog();
 
         recorder = new Recorder(callback);
         audioCalculator = new AudioCalculator();
         handler = new Handler(Looper.getMainLooper());
 
+
         textAmplitude = (TextView) findViewById(R.id.textAmplitude);
-        textDecibel = (TextView) findViewById(R.id.textDecibel);
         textFrequency = (TextView) findViewById(R.id.textFrequency);
         //textStatus = (TextView) findViewById(R.id.textStatus); // display status of program
 
-        textAverageAmp = findViewById(R.id.textAverageAmp);
-        textAverageFreq = findViewById(R.id.textAverageFreq);
-
-
         GraphView graph = (GraphView) findViewById(R.id.graph);
         initGraph(graph);
+
+        // Ask for microphone permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+
     }
 
     public void initGraph(GraphView graph) {
@@ -103,8 +109,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
-
-
     @Override
     public void onResume() {
 
@@ -112,11 +116,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             @Override
             public void run() {
                 graphLastXValue += 0.25d;
-                mSeries.appendData(new DataPoint(graphLastXValue, getRandom()), true, 100);
-                mHandler.postDelayed(this, 100);
+                mSeries.appendData(new DataPoint(graphLastXValue, getRandom()), true, myDataPoints);
+                mHandler.postDelayed(this, speed);
             }
         };
-        mHandler.postDelayed(mTimer, 500);
+        mHandler.postDelayed(mTimer, 700);
         recorder.start();
         super.onResume();
     }
@@ -130,6 +134,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     double mLastRandom = 2;
     Random mRand = new Random();
+
     private double getRandom() {
 
         //return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
@@ -137,6 +142,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * About_Dialog
      * TODO: Hyperlink to my website for a more detailed.
@@ -181,12 +187,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
 
-            // item1
             case R.id.item1:
                 aboutDialog();
                 break;
 
-            // item3
             case R.id.item2:
                 helpDialog();
                 break;
@@ -210,76 +214,182 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     /**
      * INIT AUDIO FILE SPINNER
-     *  Initialise "Audio File" the spinner for use
+     * Initialise "Audio File" the spinner for use
      */
     public void initAudioFileSpinner() {
-        Spinner audioFileSpinner = findViewById(R.id.spinner_audiofile);
-        ArrayAdapter<CharSequence> audioFileAdapter = ArrayAdapter.createFromResource(this, R.array.audioFile,
+        final ArrayAdapter<CharSequence> audioFileAdapter = ArrayAdapter.createFromResource(this, R.array.audioFile,
                 android.R.layout.simple_spinner_item);
         audioFileAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        audioFileSpinner.setAdapter(audioFileAdapter);
-        audioFileSpinner.setOnItemSelectedListener(this);
+        spinner_audiofile.setAdapter(audioFileAdapter);
+
+
+        // this is the listener, use this to read the selected item on the spinner
+        spinner_audiofile.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        audioFilePicked = 1;
+                        break;
+                    case 1:
+                        audioFilePicked = 2;
+                        break;
+                    case 2:
+                        audioFilePicked = 3;
+                        break;
+                    case 3:
+                        Toast.makeText(MainActivity.this, "Feature coming soon!", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        audioFilePicked = 1;
+                        break;
+
+                }             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
+
+
+    /**
+     * INIT AUDIO DATA RATE SPINNER
+     * Initialise "Audio Samples" the spinner for use
+     */
+    public void initDataRateSpinner() {
+        ArrayAdapter<CharSequence> audioSampleAdapter = ArrayAdapter.createFromResource(this, R.array.dataRate,
+                android.R.layout.simple_spinner_item);
+        audioSampleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_datarate.setAdapter(audioSampleAdapter);
+
+
+        // this is the listener, use this to read the selected item on the spinner
+        spinner_datarate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO: Implement the functioanlity.
+
+                switch (position) {
+                    case 0:
+                        myDataPoints = 50;
+                        break;
+                    case 1:
+                        myDataPoints = 100;
+                        break;
+                    case 2:
+                        myDataPoints = 150;
+                        break;
+                    case 3:
+                        myDataPoints = 200;
+                        break;
+                    case 4:
+                        myDataPoints = 300;
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "uhh!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+
+
 
     /**
      * INIT AUDIO SAMPLES SPINNER
-     *  Initialise "Audio Samples" the spinner for use
+     * Initialise "Audio Samples" the spinner for use
      */
-    public void initAudioSamplesSpinner() {
-        Spinner audioSampleSpinner = findViewById(R.id.spinner_samples);
-        ArrayAdapter<CharSequence> audioSampleAdapter = ArrayAdapter.createFromResource(this, R.array.sampleRate,
+    public void initSampleSpeedSpinner() {
+        ArrayAdapter<CharSequence> audioSampleAdapter = ArrayAdapter.createFromResource(this, R.array.speed,
                 android.R.layout.simple_spinner_item);
         audioSampleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        audioSampleSpinner.setAdapter(audioSampleAdapter);
-        audioSampleSpinner.setOnItemSelectedListener(this);
+        spinner_samplespeed.setAdapter(audioSampleAdapter);
+
+        // this is the listener, use this to read the selected item on the spinner
+        spinner_samplespeed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                switch (position) {
+                    case 0:
+                        speed = 10;
+                        break;
+                    case 1:
+                        speed = 25;
+                        break;
+                    case 2:
+                        speed = 50;
+                        break;
+                    case 3:
+                        speed = 100;
+                        break;
+                    case 4:
+                        speed = 200;
+                        break;
+                    case 5:
+                        speed = 300;
+                        break;
+                    case 6:
+                        speed = 500;
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "uhh!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
-    /**
-     * INIT AUDIO SOURCE SPINNER
-     *  Initialise "Audio Source" the spinner for use
-     */
 
 
-    /**
-     * WHEN ITEM SELECTED FOR SPINNERS
-     * @param parent
-     * @param view
-     * @param position
-     * @param id
-     */
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
 
-        audioFilePicked = text; // Set the audio file picked for player
-
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 
     /**
      * PLAY SOUND
      * When play is pressed, play the sound until it is finished or STOP is pressed
      */
     public void playSound(View v) {
-        // If created, we don't want to create another one
         if (player == null) {
 
             switch (audioFilePicked) {
-                case "20kHz Sweep":
+
+                case 1:
                     player = MediaPlayer.create(this, R.raw.twentythousand);
                     player.start();
                     break;
-                case "5kHz Tone":
+
+
+                case 2:
                     player = MediaPlayer.create(this, R.raw.fivekhz);
                     player.start();
                     break;
-                case "8kHz Tone":
+
+
+                case 3:
                     player = MediaPlayer.create(this, R.raw.eightkhz);
+                    player.start();
+                    break;
+
+                default:
+                    player = MediaPlayer.create(this, R.raw.twentythousand);
                     player.start();
                     break;
 
@@ -293,13 +403,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             });
         }
 
-
     }
 
+
+    /**
+     * STOP SOUND
+     * - Stop the sound.
+     *
+     * @param v
+     */
     public void stopSound(View v) {
         stopPlayer();
     }
 
+
+    /**
+     * STOP PLAYER
+     * - Stop the music player.
+     */
     private void stopPlayer() {
         if (player != null) {
             player.release();
@@ -308,46 +429,67 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
     }
 
+    /**
+     * STOP AUDIO
+     * - Used to stop the audio playback
+     */
+    public void stopAudio(View view) {
+        stopPlayer();
+    }
 
 
+    // FREEZE CLICKED
+    public void freezeClicked(View view) {
+
+        if (buttonFreezeClicked) {
+
+            mTimer = new Runnable() {
+                @Override
+                public void run() {
+                    graphLastXValue += 0.25d;
+                    mSeries.appendData(new DataPoint(graphLastXValue, getRandom()), true, myDataPoints);
+                    mHandler.postDelayed(this, speed);
+                }
+            };
+            mHandler.postDelayed(mTimer, 700);
+
+            // set it back to normal state
+            buttonFreezeClicked = false;
 
 
+        } else {
+            mHandler.removeCallbacks(mTimer);
+            buttonFreezeClicked = true;
+        }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////
+    }
 
+    /**
+     * CALLBACK
+     * - Used to display the amplitude and frequency onto the screen.
+     */
     private Callback callback = new Callback() {
 
         @Override
         public void onBufferAvailable(byte[] buffer) {
             audioCalculator.setBytes(buffer);
             int amplitude = audioCalculator.getAmplitude();
-            double decibel = audioCalculator.getDecibel();
             double frequency = audioCalculator.getFrequency();
 
             final String amp = String.valueOf(amplitude);
-            final String db = String.valueOf(decibel);
             final String hz = String.valueOf(frequency);
-
 
 
             handler.post(new Runnable() {
                 @Override
                 public void run() {
                     textAmplitude.setText(amp);
-                    textDecibel.setText(db);
                     textFrequency.setText(hz);
 
                 }
             });
         }
     };
-
-
-
-
-
-
-
 
 
 }
